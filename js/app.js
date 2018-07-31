eaf4d35e9b4db6e50562d4ea3cea25e9deeeaf3f
n-hdz <inhc.dev@gmai.com>
2018-07-30 23:20:46 -0500

    Section renderer logic * On home load calls Default Renderer * Default Renderer set to 'Films'(configurable) * Section Renderer calls to API; stores data in data-sets * Data-sets render HTML and enable info switching.

--- js/app.js
+++ js/app.js
@@ -2,15 +2,20 @@
 var request = new XMLHttpRequest();
 const app = document.getElementById('root');
 const banner = document.createElement('h1');
+const home = document.createElement('div');
+// Set Default Section
+const defaultSection = 'https://swapi.co/api/films/';
 
 //GET new connection
 request.open('GET', 'https://swapi.co/api/', true);
 app.appendChild(banner);
+
+/// Sets Main Menu and Default Section
 request.onload = function() {
   //JSON response parse
   var data = JSON.parse(this.response);
   //GET on SUCCESS
-  if(request.status >= 200 && request.status < 400) {
+  if(this.readyState == 4 && this.status == 200) {
     // OK status banner render
     banner.textContent = "Star Wars Data";
     //JSON data handling
@@ -27,7 +32,8 @@ request.onload = function() {
       mainMenu.appendChild(link);
     }
     app.appendChild(mainMenu);
-
+    // Render Default Section
+    renderDefault();
   //ON ERROR
   } else if(request.status >= 400) {
       // Error status banner render
@@ -35,26 +41,93 @@ request.onload = function() {
       console.log('El recurso no existe o no se encuentra disponible.');
     }
 }
-//On Home load
+ //// On Home load ////
 request.send();
+
+//Renders onLoad first Section; Can be configured to any section
+function renderDefault(){
+  //GETS Default section (Films)
+  const defaultRender = new XMLHttpRequest();
+  defaultRender.open('GET', defaultSection, true);
+  defaultRender.onload = function() {
+    var data = JSON.parse(this.response);
+    //GET on SUCCESS
+    if(this.readyState == 4 && this.status == 200) {
+      //Change if default changes
+      renderFilms(data);
+    //ON ERROR
+    } else if(request.status >= 400) {
+        // Error status
+        console.log('El recurso no existe o no se encuentra disponible.');
+      }
+  }
+  defaultRender.send();
+
+  app.appendChild(home);
+}
 
 //Handles Section API Calls
 function callerEvent(e){
   e.preventDefault();
-  var call = new XMLHttpRequest();
-  call.open('GET', this.href, true);
-  call.onload = function() {
+  var data = new XMLHttpRequest();
+  request.open('GET', this.href, true);
+  request.onload = function() {
     //JSON response parse
-    var innerData = JSON.parse(this.response);
+    var data = JSON.parse(this.response);
     //GET on SUCCESS
-    if(request.status >= 200 && request.status < 400) {
+    if(this.readyState == 4 && this.status == 200) {
       //JSON data handling
-      console.log(innerData);
+      console.log(data);
     //ON ERROR
     } else if(request.status >= 400) {
         console.log('El recurso no existe o no se encuentra disponible.');
       }
   }
   //On menu click call
-  call.send();
+  request.send();
+}
+
+//// Section Renderer ////
+function renderFilms(data) {
+  /*
+    Renders content for 'Films' Section;
+    Data is stored in data-sets to enable use across functions
+    without requiring aditional API calls nor scope errors.
+    Since none of the data is sensitive, all is forward-facing.
+  */
+  for(var film in data.results){
+    //Creates each individual card
+    var card = document.createElement('div');
+    card.setAttribute('class', 'card');
+    //Datasets
+    card.dataset.title = data.results[film].title;
+    card.dataset.episode = data.results[film].episode_id;
+    card.dataset.releaseDate = data.results[film].release_date;
+    card.dataset.crawl = data.results[film].opening_crawl;
+    card.dataset.director = data.results[film].director;
+    card.dataset.producer = data.results[film].producer;
+    //OnLoad Content set
+    //Title content
+    var title = document.createElement('h3');
+    title.textContent = card.dataset.title;
+    //Film Id Content
+    var filmId = document.createElement('h4');
+    filmId.textContent = card.dataset.episode;
+    //Realse Date Info
+    var dateOfRelease = document.createElement('p');
+    dateOfRelease.setAttribute('class', 'italic');
+    dateOfRelease.textContent = card.dataset.releaseDate;
+    //Render
+    card.appendChild(title);
+    card.appendChild(filmId);
+    card.appendChild(dateOfRelease);
+    //Click Event Handler
+    card.onclick = infoEvent;
+    home.appendChild(card);
+  }
+}
+
+//Section Extra Info Switcher
+function infoEvent(){
+  console.log("Click!");
 }
